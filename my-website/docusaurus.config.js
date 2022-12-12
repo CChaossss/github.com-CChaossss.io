736b24423186be88131da02bdba9a51096c60e50
lichao <1194800501@qq.com>
2022-12-12 19:18:41 +0800

    我的修改

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -16,15 +16,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'lichao', // Usually your GitHub org/user name.
-  projectName: 'https://cha0o0.github.io/', // Usually your repo name.
+  organizationName: 'cha0o0', // Usually your GitHub org/user name.
+  projectName: 'cha0o0.github.io/', // Usually your repo name.
   deploymentBranch:"gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
